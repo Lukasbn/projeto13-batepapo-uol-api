1110a8f90c24a1265d73731e7453884d79b485fa
LucasBento <lubentoneves@gmail.com>
2023-04-15 20:46:19 -0300

    post/messages finished

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.post('/participants', async (req, res) => {
         to: 'todos',
         text: "entra na sala...",
         type: 'status',
-        time: `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}`
+        time: `${dayjs().hour()}:${dayjs().minute() < 10 ? `0${dayjs().minute()}` : dayjs().minute()}:${dayjs().second() < 10 ? `0${dayjs().second()}` : dayjs().second()}`
     }
 
     try {
@@ -61,6 +61,49 @@ app.get('/participants', async (req, res) => {
 
         res.send(loggedParticipants)
     }catch(err){
+        return res.status(500).send(err.message)
+    }
+})
+
+app.post('/messages', async (req,res)=>{
+    const { to, text, type } = req.body
+    const { user } = req.headers
+
+    const messageSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.any().valid('message','private_message'),
+    })
+
+    const validation = messageSchema.validate(req.body)
+
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try{
+        const fromValitation = await db.collection('participants').findOne({name: user})
+
+        if(!fromValitation) return res.status(422).send('Esse usuário não está logado')
+        
+    }catch (err){
+        return res.status(500).send(err.message)
+    }
+
+    const newMessage = {
+        from: user,
+        to,
+        text,
+        type,
+        time: `${dayjs().hour()}:${dayjs().minute() < 10 ? `0${dayjs().minute()}` : dayjs().minute()}:${dayjs().second() < 10 ? `0${dayjs().second()}` : dayjs().second()}`
+    }
+    
+    try{
+        await db.collection('messages').insertOne(newMessage)
+        
+        return res.sendStatus(201)
+    }catch (err){
         return res.status(500).send(err.message)
     }
 })
